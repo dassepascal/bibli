4847784d4901f5f694fc82619a837da120848a70
dassepascal <pascaldasse56@gmail.com>
2021-01-12 11:02:02 +0100

    init URL

--- views/template.php
+++ views/template.php
@@ -18,11 +18,11 @@
     <div class="collapse navbar-collapse" id="navbarColor01">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item ">
-          <a class="nav-link" href="accueil"> Accueil</a>
+          <a class="nav-link" href=" <?= URL ?>accueil"> Accueil</a>
 
         </li>
         <li class="nav-item ">
-          <a class="nav-link" href="livres"> Livres</a>
+          <a class="nav-link" href="<?= URL ?>livres"> Livres</a>
 
         </li>
 
